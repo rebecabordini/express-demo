bc2bcaf514d38df7b7b476acaeeac6e44e2ff91d
Rebeca Bordini <rebeca.bordini@klarna.com>
2019-10-30 15:12:54 +0100

    fix syntax on ping route

--- routes/ping.js
+++ routes/ping.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.use(req, res, next => {
+router.use((req, res, next) => {
   console.log("Time:", Date.now());
   next();
 });
