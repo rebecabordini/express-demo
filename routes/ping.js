dcf005527421f640b8b0d7f6df550eefadac488d
Rebeca Bordini <rebeca.bordini@klarna.com>
2019-10-30 14:41:03 +0100

    fix eslint errors

--- routes/ping.js
+++ routes/ping.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.use((req, res, next) => {
-  console.log('Time:', Date.now());
+router.use(req, res, next => {
+  console.log("Time:", Date.now());
   next();
-})
+});
 
-router.get('/', function(req, res, next) {
-  res.send('pong');
+router.get("/", function(req, res, next) {
+  res.send("pong");
 });
 
 module.exports = router;
